69b61601c8bd00ba8f4ac2586d0977d3ab3b3663
anik804 <chakrabortyanik234@gmail.com>
2025-09-16 23:33:46 +0600

    Created How it works section for homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,8 @@ export default function RootLayout({ children }) {
       >
         <Navbar/>
         {children}
+        {/* <footer></footer> */}
+        <footer></footer>
       </body>
     </html>
   );
