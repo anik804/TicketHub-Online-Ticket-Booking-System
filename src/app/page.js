2120163cf3d1d929904212da346045fba5e32c09
nafeesalways <nafeezmy@gmail.com>
2025-09-17 01:00:05 +0600

    added branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from "@/layouts/Banner";
 import CustomerTestimonials from "@/layouts/CustomerTestimonials";
 import SpecialOffers from "@/layouts/SpecialOffers";
 
 export default function Home() {
   return (
     <main>
+      <Banner></Banner>
       <SpecialOffers/>
       <CustomerTestimonials/>
     </main>
