c86e20eb482e2e31ea46245997ae89d8fe9478d2
nafeesalways <nafeezmy@gmail.com>
2025-09-17 14:20:32 +0600

    added banner and newsletter section

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,18 @@ import HowItWorks from "@/layouts/HowItWorks";
 import Partners from "@/layouts/Partners";
 import SpecialOffers from "@/layouts/SpecialOffers";
 import WhyChooseUs from "@/layouts/WhyChooseUs";
+import Newsletter from "@/layouts/Newsletter";
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
       <Banner></Banner>
       <CategorySection></CategorySection>
       <SpecialOffers/>
       <CustomerTestimonials/>
       <WhyChooseUs></WhyChooseUs>
       <Partners></Partners>
+      <Newsletter></Newsletter>
       <HowItWorks></HowItWorks>
     </main>
   );
