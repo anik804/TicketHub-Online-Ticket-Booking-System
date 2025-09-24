6361311be8ccecf536d904f9ce1e75807093b64e
Pranoy Biswas <impranoybiswas@gmail.com>
2025-09-24 20:33:25 +0600

    add : loading and placeholder-avatar

---
+++ src/app/loading.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <main className="flex min-h-dvh w-full justify-center items-center">
+      <Image
+        src="/images/loading.svg"
+        width={500}
+        height={500}
+        alt="Loading..."
+        className="size-20 md:size-26 lg:size-32"
+      />
+    </main>
+  );
+}
