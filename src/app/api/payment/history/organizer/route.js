8629543a2304569448df3b1667b1c534e6a89ef3
Pranoy Biswas <impranoybiswas@gmail.com>
2025-10-08 14:05:11 +0600

    Change : Transection And Payment Route Change

--- src/app/api/payment/history/organizer/route.js
+++ src/app/api/payment/history/organizer/route.js
@@ -14,7 +14,7 @@ export async function GET(req) {
   try {
     const organizerEmail = session?.user.email;
 
-    const paymentTransactions = dbConnect("payment-transactions");
+    const paymentTransactions = dbConnect("ticket-payments");
 
     const transactions = await paymentTransactions
       .find({ organizerEmail })
@@ -39,39 +39,3 @@ export async function GET(req) {
   }
 }
 
-// DELETE Transaction
-export async function DELETE(req) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-  try {
-    const { searchParams } = new URL(req.url);
-    const tranId = searchParams.get("tranId");
-
-    if (!tranId) {
-      return NextResponse.json(
-        { error: "Transaction ID required" },
-        { status: 400 }
-      );
-    }
-
-    const paymentTransactions = dbConnect("payment-transactions");
-    const payment = await paymentTransactions.findOne({ tranId });
-
-    if (!payment) {
-      return NextResponse.json({ status: "NOT FOUND" }, { status: 404 });
-    }
-
-    await paymentTransactions.deleteOne({ tranId });
-
-    return NextResponse.json(payment, { status: 200 });
-  } catch (err) {
-    console.error("Fetch failed:", err);
-    return NextResponse.json(
-      { error: "Failed to fetch payment" },
-      { status: 500 }
-    );
-  }
-}
