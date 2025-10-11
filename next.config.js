8f795dfa349a56c2821f9c96fd637466d54970b1
Pranoy Biswas <impranoybiswas@gmail.com>
2025-10-11 12:17:53 +0600

    Add : Rolling Partners

--- next.config.js
+++ next.config.js
@@ -1,46 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "i.pravatar.cc",
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'i.ibb.co.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'i.ibb.co',
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "cdn-icons-png.flaticon.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "i.ibb.co",
-//       },
-//             {
-//         protocol: "https",
-//         hostname: "lh3.googleusercontent.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com", // GitHub avatars
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -53,9 +10,9 @@ const nextConfig = {
       { protocol: "https", hostname: "i.ibb.co" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "www.movieposters.com" },
     ],
+    domains: ["www.movieposters.com", "cdn.shopify.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
