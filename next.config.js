8613c20366b34f34f7ec9eb9432311bf6294997c
anik804 <chakrabortyanik234@gmail.com>
2025-10-14 20:09:04 +0600

    all package prob solved

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,11 +8,15 @@ const nextConfig = {
       { protocol: "https", hostname: "i.ibb.co" },
       { protocol: "https", hostname: "cdn-icons-png.flaticon.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "i.ibb.co" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "www.movieposters.com" },
+      { protocol: "https", hostname: "cdn.shopify.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "i.pinimg.com" },
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
     ],
-    domains: ["www.movieposters.com", "cdn.shopify.com", "images.pexels.com", "i.pinimg.com", "img.freepik.com", "cdn.pixabay.com"],
   },
 };
 
