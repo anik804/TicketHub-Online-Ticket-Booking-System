dc99475d99f3904bfb00b7ade7a81d7436c4da96
mdsabir07 <sabiruli@gmail.com>
2025-10-23 12:36:59 +0600

    Created ThemeToggle successfully

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // 👈 VERY IMPORTANT
+  safelist: [
+    'dark',
+  ],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
