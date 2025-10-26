66ba9f447bcd8fdee128e7c6b549dff8a320c950
Pranoy Biswas <impranoybiswas@gmail.com>
2025-10-27 01:12:18 +0600

    Changed : Dashboard style

---
+++ src/app/dashboard/(pages)/add-movie/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import AddEvents from "../../components/AddEvents";
+import DashboardSection from "../../components/shared/DashboardSection";
+
+function page() {
+  return (
+    <DashboardSection title="Add Event" role="movieAdmin">
+      Not ready
+    </DashboardSection>
+  );
+}
+
+export default page;
--- src/components/shared/ThemeToggler.jsx
+++ src/components/shared/ThemeToggler.jsx
@@ -13,19 +13,19 @@ export default function ThemeToggler({ className }) {
   return (
     <div
       role="button"
-      className={`${className} rounded-full size-8 flex justify-center items-start bg-base-100 border border-primary text-primary text-lg overflow-hidden cursor-pointer`}
+      className={`${className} rounded-full size-10 flex justify-center items-start  text-primary text-2xl overflow-hidden cursor-pointer text-shadow-xs`}
       title={`Toggle ${theme === "light" ? "Dark" : "Light"}`}
       onClick={toggleTheme}
     >
       <span
         className={`flex flex-col ${
-          theme === "light" ? "translate-y-0" : "-translate-y-8"
+          theme === "light" ? "translate-y-0" : "-translate-y-10"
         } transform transition-all duration-500 ease-in-out`}
       >
-        <span className="size-8 flex justify-center items-center">
+        <span className="size-10 flex justify-center items-center">
           <AiFillMoon />
         </span>
-        <span className="size-8 flex justify-center items-center">
+        <span className="size-10 flex justify-center items-center">
           <AiFillSun />
         </span>
       </span>
